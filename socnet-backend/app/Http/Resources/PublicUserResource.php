3e4fa3e709b8126f808aacb349c74f2686b08c55
elifdef <andriy.yt2018@gmail.com>
2026-01-24 15:49:40 +0200

    Backend: добавлена логіка чорного списку. Frontend: добавлено відображення статусу дружби на сторінці другого користувача і відображення статусу ЧС на сторінці користувача який кинув в ЧС

--- socnet-backend/app/Http/Resources/PublicUserResource.php
+++ socnet-backend/app/Http/Resources/PublicUserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Friendship;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,8 +15,24 @@ class PublicUserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $status = $this->getFriendshipStatus($request->user('sanctum'));
+        if ($status === 'blocked_by_target') {
+            return [
+                'id' => $this->id,
+                'username' => $this->username,
+                'first_name' => $this->first_name,
+                'last_name' => $this->last_name,
+                'avatar' => null,
+                'bio' => null,
+                'birth_date' => null,
+                'created_at' => null,
+                'is_setup_complete' => true,
+                'friendship_status' => $status,
+            ];
+        }
         return [
             // $this посилається на об'єкт User
+            'id' => $this->id,
             'username' => $this->username,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
@@ -24,6 +41,44 @@ class PublicUserResource extends JsonResource
             'created_at' => $this->created_at->format('d.m.Y'), // 05.01.2026
             'birth_date' => $this->birth_date,
             'is_setup_complete' => (bool)$this->is_setup_complete,
+            'friendship_status' => $this->getFriendshipStatus($request->user('sanctum'))
         ];
+    }
+    /**
+     * Визначає статус відносин відносно поточного авторизованого користувача.
+     */
+    protected function getFriendshipStatus($currentUser)
+    {
+        // якщо це не залогінений або це ми самі
+        if (!$currentUser || $currentUser->id === $this->id)
+            return 'noneS';
+
+        // перевіряємо обидва напрямки: А -> В або В -> А
+        $friendship = Friendship::where(function($q) use ($currentUser) {
+            $q->where('user_id', $currentUser->id)->where('friend_id', $this->id);
+        })->orWhere(function($q) use ($currentUser) {
+            $q->where('user_id', $this->id)->where('friend_id', $currentUser->id);
+        })->first();
+
+        if (!$friendship)
+            return 'none';
+
+        if ($friendship->status === Friendship::STATUS_ACCEPTED)
+            return 'friends';
+
+        if ($friendship->status === Friendship::STATUS_PENDING) {
+            if ($friendship->user_id === $currentUser->id)
+                return 'pending_sent'; // якщо А "відправив"
+            return 'pending_received'; // якщо А "отримав"
+        }
+
+        if ($friendship->status == Friendship::STATUS_BLOCKED) {
+            // я заблокував його
+            if ($friendship->user_id === $currentUser->id)
+                return 'blocked_by_me';
+            //він заблокував мене
+            return 'blocked_by_target';
+        }
+        return 'none';
     }
 }
